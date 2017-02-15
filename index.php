055fd04c9138607b202981f48eea614d6f3d5783
PhongDinh12 <dinhcongphong12@gmail.com>
2017-02-15 13:20:10 +0700

    bai lam

--- index.php
+++ index.php
@@ -1,13 +1,41 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
 <head>
-	<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <title></title>
 </head>
-<h1>this is trainning test file </h1>
+<body>
 <?php
-echo 'hello, day';
-$arrPersion = ['cars', 'test', 'com', 'spider'];
+$row = 1;
+if (($handle = fopen("list_persion.csv", "r")) !== FALSE) {
+    $i=0;
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        $all[$i] = $data;
+        $i++;
+//        $num = count($data);
+//        echo "<p> $num fields in line $row: <br /></p>\n";
+//        $row++;
+//        for ($c=0; $c < $num; $c++) {
+//            echo $data[$c] . "<br />\n";
+        }
+    function dateSort($a, $b)
+    {
+        $d1 = floatval(str_replace('-', '', $a['1']) . " $a[startTime]");
+        $d2 = floatval(str_replace('-', '', $b['1']) . " $b[startTime]");
+        return $d1 - $d2;
+    }
 
-foreach ($arrPersion as $persion) {
-	echo '<h1>' . $persion . '</h1>' . '<br>';
+    usort($all, 'dateSort');
+    echo '<pre>' . print_r($all, 1) . '</pre>';
+    fclose($handle);
+//    usort($data, 'dateSort');
+//    echo '<pre>' . print_r($data, 1) . '</pre>';
 }
 ?>
+</body>
+</html>
