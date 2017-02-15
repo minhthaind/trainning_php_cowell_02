5b9d7ee1ff9d6129f2357fd60d24e2ce0a48174d
chinhlv95 <chinhlv0095@gmail.com>
2017-02-15 23:59:51 +0700

    Add pagination -v1

--- chinhlv_read_file.php
+++ chinhlv_read_file.php
@@ -1,58 +1,39 @@
-<!DOCTYPE html>
-<head>
-  <title>PHP Basic</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <script>
-	function Sort(){
-		var rows = document.getElementById("myTable").rows;
-		for (i = 1; i < rows.length; i++)
-			for (j = i+1; j < rows.length; j++) 
-				if (rows[i].cells[4].innerHTML.toLowerCase() > rows[j].cells[4].innerHTML.toLowerCase()) {
-					for(m=0; m<rows[0].cells.length; m++){
-						tmp = rows[i].cells[m].innerHTML;
-						rows[i].cells[m].innerHTML = rows[j].cells[m].innerHTML;
-						rows[j].cells[m].innerHTML = tmp;
-					}
-				}
+<?php
+	if(isset($_GET["limit"])){
+		$limit = $_GET["limit"];
 	}
-	$( window ).on( "load", function(){
-        Sort();
-    });
-  </script>
-</head>
-<body>
-	<div class="container">
-	  	<h2 style="text-align:center;">Sort By Email</h2><br>
-	  	<table class="table" id="myTable">
-		    <thead>
-		      <tr>
-		        <th>Name</th>
-		        <th>Gender</th>
-		        <th>Birthday</th>
-		        <th>Phone</th>
-		        <th>Email</th>
-		      </tr>
-		    </thead>
-		    <tbody>
-			 	<?php
-				if (($handle = fopen("list_persion.csv", "r")) !== FALSE) {
-					$array = array();
-					while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-					    $num = count($data);
-					    echo "<tr>";
-					    for ($c=0; $c < $num; $c++) {
-					      	echo "<td>".$data[$c]."</td>";
-					    }
-					    echo "</tr>";
-					}
-					fclose($handle);
-				}
-				?>
-			</tbody>
-	</div>
-</body>
-</html>
+	else {
+		$limit = 5;
+	}
+	if(isset($_GET["page"]) && $_GET["page"]!=0){
+		$page = $_GET["page"];
+	}
+	else {
+		$page = 1;
+	}
+	$start_from = ($page - 1) * $limit;
+	$csv = array_map('str_getcsv', file('list_persion.csv'));
+	function array_sort($a, $b){
+	    if(strtolower($a[4])==strtolower($b[4])){
+	    	return 0;
+	    }
+	    return strtolower($a[4])>strtolower($b[4])?1:-1;
+	}
+	usort($csv, "array_sort");
+	$string = "";
+    for ($c=$start_from; $c < $start_from + $limit; $c++) {
+    	if($c < count($csv)){
+    		$count = count($csv[$c]);
+	    	$string .= "<tr>";
+    		for ($i=0; $i<$count; $i++){
+    			if(!empty($csv[$c])){
+		  			$string .= "<td>".(string)$csv[$c][$i]."</td>";
+		  		}
+		  	}
+	    	$string .= "</tr>";
+    	}
+    	
+    }
+    echo $string;
+?>
+
--- index.php
+++ index.php
@@ -1,13 +1,68 @@
+<!DOCTYPE html>
 <head>
-	<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <title>PHP Basic</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="public/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="public/js/bootstrap.min.js"></script>
 </head>
-<h1>this is trainning test file </h1>
-<?php
-echo 'hello, day';
-$arrPersion = ['cars', 'test', 'com', 'spider'];
-
-foreach ($arrPersion as $persion) {
-	echo '<h1>' . $persion . '</h1>' . '<br>';
-}
-?>
+<body>
+	<div class="container">
+	  	<h2 style="text-align:center;">Sort By Email</h2><br>
+	  	<table class="table table-hover" id="myTable">
+		    <thead>
+		      <tr>
+		        <th>Name</th>
+		        <th>Birthday</th>
+		        <th>Gender</th>
+		        <th>Phone</th>
+		        <th>Email</th>
+		      </tr>
+		    </thead>
+		    <tbody id="content">
+		    </tbody>
+		</table>
+	  	<?php
+			 $csv = array_map('str_getcsv', file('list_persion.csv'));
+			 $count = count($csv);
+			 $limit = 5;
+			 $total_pages = ceil($count/$limit);
+	  	?>
+			<nav aria-label="Page navigation">
+			  <ul class="pagination">
+			    <li class="disabled"><a href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>
+			    <?php
+			    	if(!empty($total_pages)):
+			    		for ($i=1; $i <= $total_pages; $i++):
+			    			if($i == 1):?>
+			    				<li class='active'  id="<?php echo $i;?>"><a href='chinhlv_read_file.php?page=<?php echo $i;?>&limit=<?php echo $limit; ?>'><?php echo $i;?></a></li>
+			    			<?php else: ?>
+			    				<li id="<?php echo $i;?>"><a href='chinhlv_read_file.php?page=<?php echo $i;?>&limit=<?php echo $limit; ?>'><?php echo $i;?></a></li>
+			    			<?php endif; ?>
+			    		<?php endfor; ?>
+			    	<?php endif; ?>
+			  </ul>
+			</nav>
+	</div>
+</body>
+<script>
+$(document).ready(function() {
+$("#content").load("chinhlv_read_file.php?page=1&limit=5");
+    $(".pagination li").on('click',function(e){
+    	e.preventDefault();
+        $(".pagination li").removeClass('active');
+        $(this).addClass('active');
+        var pageNum = this.id;
+		var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("content").innerHTML = this.responseText;
+            }
+        };
+        xmlhttp.open("GET", 'chinhlv_read_file.php?page='+pageNum+'&limit=5', true);
+        xmlhttp.send();
+    });
+    });
+</script>
+</html>
