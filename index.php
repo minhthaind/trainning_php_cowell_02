de7f64e62f915a1201d8e257b5cca3c4b6fb00f4
Dung Tham <dungthamkimgmail.com>
2017-02-15 15:32:59 +0700

    BTVN

--- index.php
+++ index.php
@@ -1,13 +1,59 @@
+<!DOCTYPE html>
+<html>
 <head>
-	<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+	<style type="text/css">
+		.dm{
+			text-align: center;
+		}
+	</style>
 </head>
-<h1>this is trainning test file </h1>
+<body>
 <?php
-echo 'hello, day';
-$arrPersion = ['cars', 'test', 'com', 'spider'];
-
-foreach ($arrPersion as $persion) {
-	echo '<h1>' . $persion . '</h1>' . '<br>';
+function cmp($a, $b)
+{
+    if($a[3]==$b[3]){
+    	return 0;
+    }
+    return $a[3]>$b[3]?-1:1;
 }
+$csv = array_map('str_getcsv', file('list_persion.csv'));
+usort($csv, "cmp");
+
+
+
+// for($i=0;$i<count($csv);$i++){
+// 	print_r($csv[$i]);
+// 	echo "<br/>";
+// }
+
 ?>
+
+<table border="2" align="center">
+<tr class="dm">
+	<td>Name</td>
+	<td>Birth</td>
+	<td>Gender</td>
+	<td>Number</td>
+	<td>Email</td>
+</tr>
+
+	<?php
+		for($i=0;$i<count($csv);$i++){
+			echo "<tr class='dm'>";
+			echo "<td>".$csv[$i][0]."</td>";
+			echo "<td>".$csv[$i][1]."</td>";
+			echo "<td>".$csv[$i][2]."</td>";
+			echo "<td>".$csv[$i][3]."</td>";
+			echo "<td>".$csv[$i][4]."</td>";
+			echo "</tr>";
+		}
+	?>
+
+</table>
+	
+</body>
+</html>
