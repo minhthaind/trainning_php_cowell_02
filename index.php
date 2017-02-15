5c7b543230dccf3fc85519282683814e1c5e0848
Huyen Bui <bui.khanh.huyen@framgia.com>
2017-02-15 15:39:15 +0700

    B02_HuyenBK

--- index.php
+++ index.php
@@ -2,12 +2,46 @@
 	<!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 </head>
-<h1>this is trainning test file </h1>
-<?php
-echo 'hello, day';
-$arrPersion = ['cars', 'test', 'com', 'spider'];
-
-foreach ($arrPersion as $persion) {
-	echo '<h1>' . $persion . '</h1>' . '<br>';
+<h1>List</h1>
+<?php 
+$row = 1;
+if($handle = fopen("list_persion.csv","r")){
+  $i = 0;
+  while($data = fgetcsv($handle,1000,',')){
+    $all[$i] = $data;
+    //print_r($data);
+    $i++;
+  }
+  function cmp($a,$b){
+    return strcmp($a[0], $b[0]);
+  }
+  uasort($all, 'cmp');
+  fclose($handle);
 }
 ?>
+<body>
+	<div class="container">
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Gender</th>
+          <th>Date of Birth</th>
+          <th>Phone Number</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+          foreach($all as $value){ 
+            echo "<tr>";
+            foreach($value as $vl){
+              echo "<td>$vl</td>";
+            }
+            echo "</tr>";
+          }
+        ?>  
+      </tbody>
+    </table>
+  </div>
+</body>
