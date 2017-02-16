b6e3de5d94aa73b43c8e032b6990055abbc66b8e
hopcv58 <anduin1995@gmail.com>
2017-02-16 13:46:54 +0700

    Test Source Tree

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <head>
-	<!-- Latest compiled and minified CSS -->
+	<!-- CSS testing -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 </head>
 <h1>User list</h1>
