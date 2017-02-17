6dde396de25f5bc8ad0abb370e835a8a9027fefe
Huyen Bui <bui.khanh.huyen@framgia.com>
2017-02-17 14:17:28 +0700

    Pagination_HuyenBk

--- index.php
+++ index.php
@@ -1,47 +1,102 @@
-<head>
-	<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-</head>
-<h1>List</h1>
-<?php 
-$row = 1;
-if($handle = fopen("list_persion.csv","r")){
-  $i = 0;
-  while($data = fgetcsv($handle,1000,',')){
-    $all[$i] = $data;
-    //print_r($data);
-    $i++;
-  }
-  function cmp($a,$b){
-    return strcmp($a[0], $b[0]);
-  }
-  uasort($all, 'cmp');
-  fclose($handle);
-}
-?>
-<body>
-	<div class="container">
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Gender</th>
-          <th>Date of Birth</th>
-          <th>Phone Number</th>
-          <th>Email</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
-          foreach($all as $value){ 
-            echo "<tr>";
-            foreach($value as $vl){
-              echo "<td>$vl</td>";
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>DSSV</title>
+
+    <!-- Bootstrap -->
+    <link href="public/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <h1 class="text-center">Danh sach</h1>
+
+  <body>
+  	<div class="container">
+      <table>
+        <thead>
+          <tr>
+            <th width="25%">Name</th>
+            <th width="15%">DoB</th>
+            <th width="15%">Gender</th>
+            <th width="20%">Phone Number</th>
+            <th width="25%">Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+            $total_rows = 0; 
+            if($handle = fopen("list_persion.csv","r")){
+              $i = 0;
+              while($data = fgetcsv($handle, 1000, ",")){
+                $arr[$i] = $data;
+                $i++;
+              }
+              function cmp($a,$b){
+                return strcmp($a[0], $b[0]);
+              }
+              uasort($arr, 'cmp');
+              fclose($handle);
             }
-            echo "</tr>";
-          }
-        ?>  
-      </tbody>
-    </table>
-  </div>
-</body>
+              $per_page = 5;
+              $total_rows = count($arr);
+              $max_pages = ceil($total_rows/$per_page);
+              $current_page = isset($_GET['page']) ? $_GET['page'] : 1;
+              $current_page = ($total_rows > 0) ? min($max_pages, $current_page) : 1;
+              $start = $current_page * $per_page - $per_page;
+              $slice = array_slice($arr, $start, $per_page);
+              foreach ($slice as $key => $value) {
+                ?>
+                <tr>
+                  <td><?php echo $value[0]; ?></td>
+                  <td><?php echo $value[1]; ?></td>
+                  <td><?php echo $value[2]; ?></td>
+                  <td><?php echo $value[3]; ?></td>
+                  <td><?php echo $value[4]; ?></td>
+                </tr>                  
+                <?php
+              }
+              ?>
+        </tbody>
+      </table>
+    </div>
+    <div class="row">
+      <div class="col-md-3 col-md-offset-4">
+        <ul class="pagination">
+          <?php
+            if($current_page > 1){
+          ?>
+            <li><a href="index.php?page=<?php echo ($current_page-1); ?>"><span aria-hidden="true">&laquo;</span></a></li>
+          <?php
+            }
+          ?>
+          <?php
+            for($i=0; $i < $max_pages; $i++){
+          ?>
+            <li><a href="index.php?page=<?php echo ($i+1); ?>"><?php echo ($i+1); ?></a></li>
+          <?php
+            }
+          ?>
+          <?php
+            if ($current_page < $max_pages) {
+          ?>
+            <li><a href="index.php?page=<?php echo ($current_page+1); ?>"><span aria-hidden="true">&raquo;</span></a></li>
+          <?php
+            }
+          ?>
+        </ul>
+      </div>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+      <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="public/js/bootstrap.min.js"></script>
+  </body>
+</html>
