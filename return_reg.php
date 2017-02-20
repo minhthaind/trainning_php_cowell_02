8ecfb1073821edf916dae230cc1baea75bec1d3e
ngoannguyen <ngoan.ptit36@gmail.com>
2017-02-20 14:49:13 +0700

    no message

--- return_reg.php
+++ return_reg.php
@@ -1,32 +1,4 @@
    <?php 
-   /*if(isset($_POST['name'])&& isset($_POST['birthday']) &&isset($_POST['email']) && isset($_POST['phone_number']) &&isset($_POST['key_persion'])&&isset($_POST['sex']))
-   {
-   	$name=$_POST['name'];
-   	$birthday=$_POST['birthday'];
-   	$sex=$_POST['email'];
-   	$phone_number=$_POST['phone_number'];
-   	$email=$_POST['email'];
-   }
-   	if(checkemail($email)&& checkbirthday($birthday)&& checkname($name) && checkphone($phone) && $sex!="")
-   	//{function test_input($data){
-   		$arr1 = array($name ,$birthday,$email,$phone_number,$key_persion,$sex );
-   		$arr=array();
-   		$row=1;
-   		if (($handle = fopen("list_persion.csv", "r")) !== FALSE) {
-   			while (($data= fgetcsv($handle, 1000,",")) !== FALSE){ 
-   				array_push($arr, $data);
-   				$row      ;
-   			}
-   			array_push($arr, $arr1);
-   			$fp = fopen('list_persion', 'w');
-   			foreach ($arr as $fields) {
-   				fputcsv($fp, $fields);
-   			}
-   
-   			fclose($fp);
-   	//}
-      } */ 
-    //$pattern = "/^[A-Za-z0-9_\.]{6,32}@([a-zA-Z0-9]{2,12})(\.[a-zA-Z]{2,12})+$/";
 	$name = test_input($_POST['name']);
 	$birthday = test_input($_POST['birthday']);
 	$sex = test_input($_POST['sex']);
